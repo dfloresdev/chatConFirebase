063bf4217865723ea54a0e3582a1cc472ae6d156
davidfm <davisolar94@gmail.com>
2017-05-03 10:05:58 -0500

    se realizo la insercion de chats en la ventana Salas

--- js/principal.js
+++ js/principal.js
@@ -4,8 +4,8 @@ function registrarUsuario(email, password, idMensaje)
     firebase.auth().createUserWithEmailAndPassword(email, password).then(
             function (usuario)
             {
+                console.log("si entra a agregar usuario");
                 console.log(usuario);
-                
                 var usr = {};
                 usr.correo = usuario.email;
                 firebase.database().ref().child("usuario").push().set(usr);
@@ -33,7 +33,7 @@ function cerrarSesion()
 
 function iniciarSesion(email, password)
 {
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -45,7 +45,7 @@ function inicioConFacebook()
 {
     var provider = new firebase.auth.FacebookAuthProvider();
     provider.addScope('user_birthday');
-    
+
     firebase.auth().signInWithPopup(provider).then(function (result) {
         // This gives you a Facebook Access Token. You can use it to access the Facebook API.
         var token = result.credential.accessToken;
@@ -53,20 +53,20 @@ function inicioConFacebook()
         var user = result.user;
         console.log("si entra");
         console.log(user);
-        
+
         var usr = {};
         usr.correo = user.email;
         usr.nombre = user.displayName;
         usr.photo = user.photoURL;
         usr.token = token;
-        
-        firebase.database().ref().child("usuario").child(user.uid).set(usr, function(error)
+
+        firebase.database().ref().child("usuario").child(user.uid).set(usr, function (error)
         {
 //            console.log(error);
-            window.location="salas.html";
+            window.location = "salas.html";
         });
-        
-        
+
+
         // ...
     }).catch(function (error) {
         // Handle Errors here.
@@ -78,5 +78,38 @@ function inicioConFacebook()
         var credential = error.credential;
         $("#mensaje").html(error.message + error.code);
         // ...
+    });
+}
+
+function consultarSalas()
+{
+    firebase.database().ref().child("salas").on("value", function (salasSnapShot)
+    {
+        $("#salas").html("");
+        salasSnapShot.forEach(function (sala)
+        {
+            //este es para limpiar y no las duplique
+
+
+            console.log(sala.val());
+
+            $("#salas").append(
+//                    '<div class = "item">'+
+//                    '<img class = "ui small image" src = "' + sala.val().urlFoto + '" />'+
+//                    '<div class = "content">'+
+//                    '<a class = "header">Nombre de la sala: ' + sala.val().nombreSala + ' </a>'+
+//                    '<div class = "description">Participantes: ' + sala.val().numPersonas + '</div>'+
+//                    '</div>'+
+//                    '</div>'
+
+                    '<div class = "item">' +
+                    '<img class = "ui avatar tiny image" src = "' + sala.val().urlFoto + '" >' +
+                    '<div class = "content" >' +
+                    '<div class = "header" >Nombre de sala: ' + sala.val().nombreSala + '</div>' +
+                    'Participantes: ' + sala.val().numPersonas +
+                    '</div>' +
+                    '</div>'
+                    );
+        });
     });
 }
