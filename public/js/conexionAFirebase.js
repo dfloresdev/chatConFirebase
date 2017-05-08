2c361fbed2986bca13702d5ef5507e36ec79a9ee
davidfm <davisolar94@gmail.com>
2017-05-08 09:37:36 -0500

    se hosteo en firebase

---
+++ public/js/conexionAFirebase.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB__00Fkhekv25MEf2K3DcjsMoldDUt7A4",
+    authDomain: "david-f1100.firebaseapp.com",
+    databaseURL: "https://david-f1100.firebaseio.com",
+    projectId: "david-f1100",
+    storageBucket: "david-f1100.appspot.com",
+    messagingSenderId: "345968204304"
+  };
+  firebase.initializeApp(config);
